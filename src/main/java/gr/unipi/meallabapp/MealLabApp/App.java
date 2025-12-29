c076e1111d71c3e2f82584aabd87f3e9c7102ae2
elinapap4 <elina.s.papadopoulou@gmail.com>
2025-12-29 15:39:55 +0200

    Update App & MainScene

--- src/main/java/gr/unipi/meallabapp/MealLabApp/App.java
+++ src/main/java/gr/unipi/meallabapp/MealLabApp/App.java
@@ -4,22 +4,35 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+//The App class extends the JavaFX Application class.
 public class App extends Application {
+	
+    // Static field to allow other classes to access the main window
+    public static Stage window;
 
 	Scene mainScene;
 	
+	// We override the start method of the abstract Application class.
     @Override
     public void start(Stage primaryStage) {
+    	
+    	// We assign the primaryStage to our static window variable.
+        window = primaryStage;
 
+    	// We create an object using the MainSceneCreator constructor.
     	MainSceneCreator mainSceneCreator = new MainSceneCreator(650,500);
+    	
+    	// We call the createScene() method to build the scene.
         mainScene = mainSceneCreator.createScene();
-
+        
+        
+      // We use Java-fx methods of the Stage class to configure the main window
     	primaryStage.setTitle("Meal Lab App");
         primaryStage.setScene(mainScene);
         primaryStage.show();
     }
 
+    // The main method that launches the app.
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/gr/unipi/meallabapp/MealLabApp/MainSceneCreator.java
+++ src/main/java/gr/unipi/meallabapp/MealLabApp/MainSceneCreator.java
@@ -1,60 +1,93 @@
 package gr.unipi.meallabapp.MealLabApp;
 
-import gr.unipi.meallab.MealApiClient;
-import gr.unipi.meallab.Recipe; 
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
+//The class implements the EventHandler interface handle mouse clicks.
 public class MainSceneCreator implements EventHandler<MouseEvent> {
+	
+	//Fields for window dimensions
+	int width, height;
+	
+	//Fields that represent JavaFX UI components (Nodes)
     GridPane root;
-    int width, height;
+    Label welcomeLabel;
+    Button searchMenuBtn;
+    Button listsMenuBtn;
+    Button randomMenuBtn;
     
-    TextField searchInput;
-    Button searchBtn;
-    ListView<String> searchList;
-    Recipe[] searchResults; 
-    
+ // Constructor of the class that receives the window dimensions.
     public MainSceneCreator(int width, int height) {
         this.width = width;
         this.height = height;
+        
+        // Initialize the GridPane and set centering and gaps between cells
         root = new GridPane();
         root.setAlignment(Pos.CENTER);
         root.setHgap(10);
         root.setVgap(10);
         
-        searchInput = new TextField();
-        searchBtn = new Button("Αναζήτηση");         
-        searchList = new ListView<>();
+        // Initialize the nodes
+        welcomeLabel= new Label("Αρχικό Μενού");
+        searchMenuBtn = new Button("Αναζήτηση Συνταγής");
+        listsMenuBtn = new Button("Οι Λίστες μου");
+        randomMenuBtn = new Button("Τι να μαγειρέψω σήμερα;");
         
-        root.add(searchInput, 0, 0);
-        root.add(searchBtn, 1, 0);
-        root.add(searchList, 0, 1, 2, 1);
+       // Configure the title's appearance: set font size, center text, and match button width
+        welcomeLabel.setFont(new Font("Arial", 16));
+        welcomeLabel.setAlignment(Pos.CENTER);
+        welcomeLabel.setMinWidth(200);
         
-      
-        searchBtn.setOnMouseClicked(this);
+        // Set minimum size for the buttons.
+        searchMenuBtn.setMinSize(200, 40);
+        listsMenuBtn.setMinSize(200, 40);
+        randomMenuBtn.setMinSize(200, 40);
+        
+        
+        // Position nodes in the grid: (column, row)
+        root.add(welcomeLabel, 0, 0);
+        root.add(searchMenuBtn, 0, 5);
+        root.add(listsMenuBtn, 0, 7);
+        root.add(randomMenuBtn, 0, 9);
+        
+        // Connect all buttons to this class so that the handle() method runs when clicked
+        searchMenuBtn.setOnMouseClicked(this);
+        listsMenuBtn.setOnMouseClicked(this);
+        randomMenuBtn.setOnMouseClicked(this);
     }
     
+ // Create and return the Scene object
     public Scene createScene() {
         Scene s = new Scene(root, width, height); 
         return s;
     }
     
+ // We override the handle method from the EventHandler interface
     @Override
     public void handle(MouseEvent event) {
-        if (event.getSource() == searchBtn) {
-            searchResults = MealApiClient.searchRecipes(searchInput.getText());
-            searchList.getItems().clear();
-            if (searchResults != null) {
-                for (int i = 0; i < searchResults.length; i++) {
-                    searchList.getItems().add(searchResults[i].getTitle());
-                }
-            }
+        if (event.getSource() == searchMenuBtn) {
+        	// 1. Create an object of the search scene creator class
+            SearchSceneCreator searchCreator = new SearchSceneCreator(width, height);
+            
+            // 2. Call the createScene() method to get the new scene
+            Scene searchScene = searchCreator.createScene();
+            
+            // 3. Set the search scene to the main window (stage)
+            App.window.setScene(searchScene);
         }
-    } 
-} 
+        else if (event.getSource() == randomMenuBtn) {
+            
+            System.out.println("Random Recipe clicked!");
+        }
+        else if (event.getSource() == listsMenuBtn) {
+           
+            System.out.println("My Lists clicked!");
+        }
+    }
+}
